9231b243e0f0c4a7cc3c5931822cc89518db9e3c
John Levermore <john.levermore@zhibek.com>
2023-10-31 14:51:42 +0000

    Include railway tiles in map (styling TODO)

--- src/MapView.js
+++ src/MapView.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import Map, { NavigationControl } from 'react-map-gl/maplibre';
 import maplibregl from 'maplibre-gl';
+import { Protocol } from 'pmtiles';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 import mapStyle from './mapStyle.json';
+const { NODE_ENV, PUBLIC_URL } = process.env;
+if (NODE_ENV === 'development') {
+  mapStyle.sources.railways.url = mapStyle.sources.railways.url.replace('/files/', `${PUBLIC_URL}/files/`);
+}
 
 const MapView = () => {
+  useEffect(() => {
+    let protocol = new Protocol();
+    maplibregl.addProtocol('pmtiles', protocol.tile);
+    return () => {
+      maplibregl.removeProtocol('pmtiles');
+    };
+  }, []);
+
   return (
     <Map
       initialViewState={{
